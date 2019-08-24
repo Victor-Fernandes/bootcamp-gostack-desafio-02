9f2f2cc7bdb312e8c5aa5949500286b61d269bfd
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-08-23 22:18:48 -0300

    Configuração inicial do projeto.

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [ 'airbnb-base','prettie'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "class-method-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
+  },
+};
